5a73720c73b289fe3f1d72101023600fc463e4b6
kangaree <garyzkang@gmail.com>
2024-05-28 11:32:04 -0400

    Add stream for tickets For now, use New column Flatten ticket (user id and img)

--- examples/rails-react-example/app/models/ticket.rb
+++ examples/rails-react-example/app/models/ticket.rb
@@ -3,4 +3,6 @@ class Ticket < ApplicationRecord
   validates :ticket_type, presence: true
   validates :user_name, presence: true
   validates :user_image, presence: true
+
+  broadcasts_to ->(ticket) { ["tickets"] }, inserts_by: :append, partial: "shared/ticket_escalations_line_item"
 end
